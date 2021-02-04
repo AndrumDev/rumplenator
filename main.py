a1cecc5a90866078a7fbf4715bd22d471ccaee99
tabi-katze <78348655+tabi-katze@users.noreply.github.com>
2021-02-04 14:20:07 +0200

    improve local setup (#8)
    * add requirements.txt
    
    * use dotenv for env vars
    
    * add flake8 config, add pytest
    
    * get test setup working
    
    use absolute imports because they work, add test config, add a couple of tests
    
    * local setup instructions
    
    * add .travis.yml
    
    * add travis env
    
    * build plz
    
    * prevent .test.env vars from being overridden
    
    * use global env vars
    
    to prevent travis running multiple jobs
    
    * add build status to README \o/
    
    * fix strings okay

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from bot.rumplenator import Rumplenator
+from dotenv import load_dotenv
+
+# for more options see https://pypi.org/project/python-dotenv/
+load_dotenv()
 
 bot = Rumplenator()
 bot.run()

6eb948c59e213f90a3f63da565fabad9efb99c72
Tabi <t.goodall@email.com>
2021-02-09 19:56:10 +0200

    config setup

--- bot/commands/command_logic/dyson.py
+++ bot/commands/command_logic/dyson.py
@@ -1,19 +1,21 @@
+from config import get_config
 from collections import defaultdict
 from random import randint
-import sys
 
 
-def __get_dyson_data():
+dyson_file_path = get_config().get('resource_dir') / 'dyson_pog.txt'
+
+
+def __get_dyson_data() -> dict, list(str):
     quotes_dict = __read_dyson_file()
     quote_params = list(quotes_dict.keys())
     return quotes_dict, quote_params
 
 
-def __read_dyson_file():
-    dysontext = sys.argv[1]
-    f = open(dysontext, "r")
+def __read_dyson_file() -> dict:
+    dyson_file = open(dyson_file_path, "r")
     quotesdict = defaultdict(lambda: [])
-    for line in f:
+    for line in dyson_file:
         line = line.split('#')[0].strip()
         if line == '':
             continue
@@ -25,11 +27,11 @@ def __read_dyson_file():
     return quotesdict
 
 
-def get_dyson_message(param=''):
+def get_dyson_message(param='') -> str:
     quotes, params = __get_dyson_data()
     if param == '':
         param = params[randint(0, len(params)-1)]
     if param in params:
-        return (' '.join(quotes[param][randint(0, len(quotes[param])-1)])).encode("utf-8").decode("utf-8")
+        return ' '.join(quotes[param][randint(0, len(quotes[param])-1)])
     else:
         return 'We currently do not have that in stock. Please choose from our current range: hand, heat, dry, fan, style, and vac.'
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from bot.rumplenator import Rumplenator
-from dotenv import load_dotenv
 
-# for more options see https://pypi.org/project/python-dotenv/
-load_dotenv()
 
 bot = Rumplenator()
 bot.run()

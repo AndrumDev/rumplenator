10d53ae145f80f958b430e3b5ef6517f4b8738f8
tabi-katze <78348655+tabi-katze@users.noreply.github.com>
2021-02-19 14:33:08 +0100

    set up app config and add dyson texts (#16)
    * add requirements.txt
    
    * use dotenv for env vars
    
    * add flake8 config, add pytest
    
    * get test setup working
    
    use absolute imports because they work, add test config, add a couple of tests
    
    * local setup instructions
    
    * add .travis.yml
    
    * add travis env
    
    * build plz
    
    * prevent .test.env vars from being overridden
    
    * use global env vars
    
    to prevent travis running multiple jobs
    
    * add build status to README \o/
    
    * fix strings okay
    
    * discord-travis integration
    
    * config setup
    
    * refactor dyson, add tests
    
    * fix resource path
    
    * fix .gitignore

--- bot/commands/command_logic/dyson.py
+++ bot/commands/command_logic/dyson.py
@@ -1,19 +1,16 @@
+from config import get_config
+from typing import Dict, List, Tuple
 from collections import defaultdict
 from random import randint
-import sys
 
 
-def __get_dyson_data():
-    quotes_dict = __read_dyson_file()
-    quote_params = list(quotes_dict.keys())
-    return quotes_dict, quote_params
+dyson_file_path = get_config().get('resource_dir') / 'dyson_pog.txt'
 
 
-def __read_dyson_file():
-    dysontext = sys.argv[1]
-    f = open(dysontext, "r")
+def __read_dyson_file() -> Dict:
+    dyson_file = open(dyson_file_path, "r")
     quotesdict = defaultdict(lambda: [])
-    for line in f:
+    for line in dyson_file:
         line = line.split('#')[0].strip()
         if line == '':
             continue
@@ -25,11 +22,12 @@ def __read_dyson_file():
     return quotesdict
 
 
-def get_dyson_message(param=''):
-    quotes, params = __get_dyson_data()
-    if param == '':
-        param = params[randint(0, len(params)-1)]
-    if param in params:
-        return (' '.join(quotes[param][randint(0, len(quotes[param])-1)])).encode("utf-8").decode("utf-8")
+def get_dyson_message(category=None) -> str:
+    quotes = __read_dyson_file()
+    categories = list(quotes.keys())
+    if not category:
+        category = categories[randint(0, len(categories)-1)]
+    if category in categories:
+        return ' '.join(quotes[category][randint(0, len(quotes[category])-1)])
     else:
         return 'We currently do not have that in stock. Please choose from our current range: hand, heat, dry, fan, style, and vac.'

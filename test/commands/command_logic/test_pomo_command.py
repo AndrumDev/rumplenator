23dc36c25b117b90b4b0bd809754ac4c591afd89
twitchett <twitchett@users.noreply.github.com>
2021-03-18 15:24:30 +0100

    we had a test for that

--- test/commands/command_logic/test_pomo_command.py
+++ test/commands/command_logic/test_pomo_command.py
@@ -14,7 +14,7 @@ def test_get_pom_with_one_arg():
     args = ['10']
     work_time, break_time, num_sessions, topic = __get_pom_args(args)
     assert work_time == 10
-    assert break_time == 0
+    assert break_time == None
     assert num_sessions == 1
     assert topic == ''
 
